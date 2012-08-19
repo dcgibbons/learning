0ee4e7da194fd828fc92fb92893a782db110fb03
Chad Gibbons <dcgibbons@gmail.com>
2012-08-19 12:15:23 -0600

    adding some python stuff

---
+++ python/codekata/karate_chop.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+#
+# Code Kata Two - Karate Chop
+# http://codekata.pragprog.com/2007/01/kata_two_karate.html
+#
+# Chad Gibbons
+# dcgibbons@gmail.com
+# August 19, 2012
+#
+
+import unittest
+
+def chop(key, l):
+    lower = 0
+    upper = len(l) - 1
+    
+    while (upper >= lower):
+        mid = (lower + upper) / 2
+        if (key < l[mid]):
+            upper = mid - 1
+        elif (key > l[mid]):
+            lower = mid + 1
+        else:
+            return mid
+    return -1
+
+
+def chop2(key, l):
+    def chopit(key, l, lower, upper):
+        if (upper < lower):
+            return -1
+        else:
+            mid = (lower + upper) / 2
+            if (key < l[mid]):
+                return chopit(key, l, lower, mid - 1)
+            elif (key > l[mid]):
+                return chopit(key, l, mid + 1, upper)
+            else:
+                return mid
+    return chopit(key, l, 0, len(l) - 1)
+
+
+class TestKarateChop(unittest.TestCase):
+    def template(self, fn):
+        self.assertEqual(-1, fn(3, []))
+        self.assertEqual(-1, fn(3, [1]))
+        self.assertEqual(0,  fn(1, [1]))
+
+        self.assertEqual(0,  fn(1, [1, 3, 5]))
+        self.assertEqual(1,  fn(3, [1, 3, 5]))
+        self.assertEqual(2,  fn(5, [1, 3, 5]))
+        self.assertEqual(-1, fn(0, [1, 3, 5]))
+        self.assertEqual(-1, fn(2, [1, 3, 5]))
+        self.assertEqual(-1, fn(4, [1, 3, 5]))
+        self.assertEqual(-1, fn(6, [1, 3, 5]))
+
+        self.assertEqual(0,  fn(1, [1, 3, 5, 7]))
+        self.assertEqual(1,  fn(3, [1, 3, 5, 7]))
+        self.assertEqual(2,  fn(5, [1, 3, 5, 7]))
+        self.assertEqual(3,  fn(7, [1, 3, 5, 7]))
+        self.assertEqual(-1, fn(0, [1, 3, 5, 7]))
+        self.assertEqual(-1, fn(2, [1, 3, 5, 7]))
+        self.assertEqual(-1, fn(4, [1, 3, 5, 7]))
+        self.assertEqual(-1, fn(6, [1, 3, 5, 7]))
+        self.assertEqual(-1, fn(8, [1, 3, 5, 7]))
+
+    def test_chop(self):
+        self.template(chop)
+
+    def test_chop2(self):
+        self.template(chop2)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ python/hello.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+#
+# Hello World for Python
+#
+# Chad Gibbons
+# dcgibbons@gmail.com
+# August 19, 2012
+#
+
+if __name__ == '__main__':
+    print "Hello, world!"
