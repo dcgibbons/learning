54e63f09222990861817562b72702ee6793ece23
Chad Gibbons <dcgibbons@gmail.com>
2012-09-07 09:14:26 -0600

    added solution for exercise 1.3 from Programming Pearls

---
+++ pearls/chapter01/exer03.c
@@ -0,0 +1,55 @@
+/*
+ * exer03.c
+ * Exercise 1.3 from Programming Pearls
+ *
+ * Chad Gibbons
+ * September 6, 2012
+ */
+
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_VALUE 10000000
+
+int main(int argc, char *argv[])
+{
+    // allocate a bit vector large enough to hold MAX_VALUE. We can't assume 
+    // this platform has 8-bits in an unsigned char, so we use the CHAR_BIT 
+    // limit to correctly determine how many bits are in each char type.
+    unsigned char* bits = calloc(MAX_VALUE / CHAR_BIT, sizeof(unsigned char));
+    if (!bits)
+    {
+        fprintf(stderr, "unable to allocate %lu bytes: %s\n", 
+                MAX_VALUE / CHAR_BIT * sizeof(unsigned char), 
+                strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[BUFSIZ];
+    while (fgets(buffer, sizeof(buffer), stdin) != NULL)
+    {
+        int n = atoi(buffer);
+        assert (n > 0 && n < MAX_VALUE);
+
+        int byte = n / CHAR_BIT;
+        int bit = n % CHAR_BIT;
+        bits[byte] |= 1 << bit;
+    }
+
+    for (int i = 0; i < MAX_VALUE; i++)
+    {
+        int byte = i / CHAR_BIT;
+        int bit = i % CHAR_BIT;
+        if (bits[byte] & 1 << bit)
+        {
+            fprintf(stdout, "%07d\n", i);
+        }
+    }
+
+    free(bits);
+    return 0;
+}
---
+++ pearls/chapter01/gen_numbers.c
@@ -0,0 +1,64 @@
+/*
+ * gen_numbers.c
+ * Programming Pearls, Chapter 1
+ *
+ * Generates a list of 7-digit numbers that are semantically valid North 
+ * America Numbering Plan telephone numbers. Since we don't need an area code 
+ * for the problem statement, we just need to make sure that the central office 
+ * code begins with a digit between 2 and 9.
+ *
+ * Chad Gibbons
+ * September 6, 2012
+ */
+
+#include <errno.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_VALUE       10000000
+#define MAX_NUMBERS     1000000
+
+int main(void)
+{
+    /* use a bit vector here to ensure we don't generate the same telephone 
+     * number more than once.
+     */
+    unsigned char* bits = calloc(MAX_VALUE / CHAR_BIT, sizeof(unsigned char));
+    if (!bits)
+    {
+        fprintf(stderr, "Unable to allocate %lu bytes: %s\n",
+                MAX_VALUE / CHAR_BIT * sizeof(unsigned char),
+                strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    (void)srand(time(NULL));
+
+    for (int i = 0; i < MAX_NUMBERS; i++)
+    {
+        while (true)
+        {
+            const int n = (rand() % MAX_NUMBERS) + 2000000;
+            const int byte = n / CHAR_BIT;
+            const int bit = n % CHAR_BIT;
+            if (bits[byte] & 1 << bit)
+            {
+                // generate another number if we've already done this one
+                continue;
+            }
+            else
+            {
+                bits[byte] |= 1 << bit;
+                fprintf(stdout, "%07d\n", n);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
+
