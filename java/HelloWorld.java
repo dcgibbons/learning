52109982e77e65fc7e406cd22dfa1fb7e9a8cc3b
Chad Gibbons <dcgibbons@gmail.com>
2012-09-07 09:13:55 -0600

    added a Java hello world

---
+++ java/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * HelloWorld.java
+ *
+ * Chad Gibbons
+ * September 6, 2012
+ */
+
+public class HelloWorld
+{
+    public static void main(final String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
+
